3fc94cc4255f5e7033288fca85be64fed992c645
KayAvila <kayavila@illinois.edu>
2022-04-23 20:40:16 -0500

    Working script

--- scripts/create_book_csv.py
+++ scripts/create_book_csv.py
@@ -1,3 +1,4 @@
+import csv
 import sys
 from pprint import pprint
 from isbn_resolver.resolver import MissingDataError, NoBookDataError, ISBNResolver
@@ -30,7 +31,12 @@ if __name__ == '__main__':
     isbn_db = ISBNDBResolver(ISBNDB_DB, ISBNDB_API_KEY)
 
     bad_isbns = []
-    with open(INPUT_FILE) as infile:
+    with open(INPUT_FILE) as infile, open(OUTPUT_FILE, 'w', encoding='utf-8', newline='') as outfile:
+        csv_outfile = csv.writer(outfile)
+        heading = ['isbn', 'ol_title', 'idb_title', 'ol_author', 'idb_author', 'ol_contributor', 'ol_by',
+                   'ol_publisher', 'idb_publisher', 'ol_location', 'ol_year', 'idb_year', 'ol_pages',
+                   'idb_pages', 'idb_msrp']
+        csv_outfile.writerow(heading)
         for line in infile:
             isbn = line.rstrip()
 
@@ -42,16 +48,18 @@ if __name__ == '__main__':
                 bad_isbns.append(isbn)
                 continue
 
-            book_data = [ol_title, idb_title]
+            book_data = [isbn, ol_title, idb_title]
 
-            # Order: titles x2, author x2, publisher x2, location x2, year x2, page_count x2, ISBNDB msrp
+            # Order: titles x2, author x2, OL contributor, OL by_statement, publisher x2, OL location, year x2,
+            #          page_count x2, ISBNDB msrp
             queries = [
                 open_library.get_author,
                 isbn_db.get_author,
+                open_library.get_contributors,
+                open_library.get_by_statement,
                 open_library.get_publisher,
                 isbn_db.get_publisher,
                 open_library.get_location,
-                isbn_db.get_location,
                 open_library.get_year,
                 isbn_db.get_year,
                 open_library.get_page_count,
@@ -62,4 +70,4 @@ if __name__ == '__main__':
             for q in queries:
                 book_data.append(get_or_return_none(isbn, q))
 
-            pprint(book_data)
+            csv_outfile.writerow(book_data)
